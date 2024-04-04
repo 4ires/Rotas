0e945bdf3c4829a821f9e8d760e8825e0d36130b
4ires <132360305+4ires@users.noreply.github.com>
2024-04-04 13:18:42 -0300

    Rotas nomeadas

---
+++ lib/tela3.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'tela3.dart';
+
+class Tela3 extends StatelessWidget{
+  const Tela3 ({super.key});
+
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Segunda tela "ROTA" '),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            Navigator.popAndPushNamed(
+              context, '/',
+            );
+          },
+          child: const Text('Voltar para a tela Inicial'),
+        ),
+      ),
+    );
+  }
+}
